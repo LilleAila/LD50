8b82dac1a8b2b4089d7e8b554f162bbcb825410d
IldenH <84644748+IldenH@users.noreply.github.com>
2022-04-03 16:51:18 +0200

    Hopefully fixed conflict when merging

--- Assets/Scripts/Dam/DamScript.cs
+++ Assets/Scripts/Dam/DamScript.cs
@@ -8,6 +8,19 @@ public class DamScript : MonoBehaviour
     public int logs = 1;
     [SerializeField] float startTime = 25f;
     [SerializeField] float floodSpeed = 17f;
+    
+    [Header ("Game Over")]
+    [SerializeField] GameObject gameOverPanel;
+    [SerializeField] GameObject[] tileMaps;
+    [SerializeField] GameObject fakeWaterTileMap;
+    [SerializeField] GameObject treeSpawner;
+    [SerializeField] GameObject treeParent;
+    [SerializeField] BallController player;
+
+    private void Update()
+    {
+        Debug.Log(logs);
+    }
 
     private void Start()
     {
@@ -34,7 +47,20 @@ public class DamScript : MonoBehaviour
         stages[currentStage].SetActive(true);
     }
 
-    void GameOver() { }
+    void GameOver() 
+    { 
+        gameOverPanel.SetActive(true);
+        for (int i = 0; i < tileMaps.Length; i++)
+        {
+            Destroy(tileMaps[i]);
+        }
+        fakeWaterTileMap.SetActive(true);
+        Destroy(treeSpawner);
+        Destroy(treeParent);
+        player.enabled = false;
+
+        Destroy(gameObject);
+    }
 
     void Flood()
     {
